ea528b7b2e8805806599350805b5797ebc6b3ddc
cris <civargas5@uc.cl>
2021-04-23 12:36:13 -0400

    conflict

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }, 
-                      path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
+                      path: '', path_names: {sign_in: 'login22', sign_out: 'logout', sign_up: 'register'}
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
